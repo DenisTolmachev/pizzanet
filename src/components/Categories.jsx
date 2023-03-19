58d2da1b250a31baf2d3146dd4f74b14c9b35606
Denis <denis.tolmachev@gmail.com>
2023-03-19 19:22:21 +0200

    corrected style and added useState for button

---
+++ src/components/Categories.jsx
@@ -0,0 +1,14 @@
+export const Categories = () => {
+  return (
+    <div className='categories'>
+      <ul>
+        <li className='active'>Всі</li>
+        <li>М'ясні</li>
+        <li>Вегетаріанські</li>
+        <li>Гриль</li>
+        <li>Гострі</li>
+        <li>Закриті</li>
+      </ul>
+    </div>
+  );
+};
