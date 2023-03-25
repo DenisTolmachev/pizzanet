600fe430499f0fd46eebd9d121f4f7594f1f71c4
Denis <denis.tolmachev@gmail.com>
2023-03-25 17:58:36 +0200

    sorting and filtering products

--- src/components/Categories.jsx
+++ src/components/Categories.jsx
@@ -1,31 +1,23 @@
-import { useState } from 'react';
-
-export const Categories = () => {
-  const [activeIndex, setActiveIndex] = useState(0);
-
+export const Categories = ({ value, onChangeCategory }) => {
   const categories = [
     'Всі',
     'М`ясні',
+    'Рибні',
     'Вегетаріанські',
     'Гриль',
     'Гострі',
-    'Закриті',
   ];
-
-  const onClickCategory = index => {
-    setActiveIndex(index);
-  };
 
   return (
     <div className='categories'>
       <ul>
-        {categories.map((value, i) => (
+        {categories.map((categoryName, i) => (
           <li
             key={i}
-            onClick={() => onClickCategory(i)}
-            className={activeIndex === i ? 'active' : ''}
+            onClick={() => onChangeCategory(i)}
+            className={value === i ? 'active' : ''}
           >
-            {value}
+            {categoryName}
           </li>
         ))}
       </ul>
