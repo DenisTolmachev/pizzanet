e098184aafbdb4bc3b98a21136cf6f6d5a1eb0f8
Denis <denis.tolmachev@gmail.com>
2023-03-26 18:49:04 +0300

    created search input & styling him

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,5 +1,6 @@
 import logo from '../assets/img/logo.png';
 import { Link } from 'react-router-dom';
+import { Search } from './Search/Search';
 
 export const Header = () => {
   return (
@@ -14,7 +15,7 @@ export const Header = () => {
             </div>
           </div>
         </Link>
-
+        <Search />
         <div className='header__cart'>
           <Link to='/cart' className='button button--cart'>
             <span>520 ₴</span>
