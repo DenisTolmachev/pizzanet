7875d56c9c5c0e11c4654269888570355fe3238a
Denis <denis.tolmachev@gmail.com>
2023-04-09 19:52:34 +0300

    added debounce logic

--- src/components/Search/Search.jsx
+++ src/components/Search/Search.jsx
@@ -1,22 +1,38 @@
 import styles from './Search.module.scss';
-import { useContext, useRef } from 'react';
+import { useCallback, useContext, useRef, useState } from 'react';
 import { SearchContext } from '../../App';
+import debounce from 'lodash.debounce';
 
 export const Search = () => {
-  const { searchValue, setSearchValue } = useContext(SearchContext);
+  const [value, setValue] = useState('');
+  const { setSearchValue } = useContext(SearchContext);
   const inputRef = useRef();
 
   const onClickClear = () => {
     setSearchValue('');
+    setValue('');
     inputRef.current.focus();
+  };
+
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  const updateSearchValue = useCallback(
+    debounce(str => {
+      setSearchValue(str);
+    }, 250),
+    []
+  );
+
+  const onChangeInput = event => {
+    setValue(event.target.value);
+    updateSearchValue(event.target.value);
   };
 
   return (
     <div className={styles.root}>
       <input
         ref={inputRef}
-        value={searchValue}
-        onChange={event => setSearchValue(event.target.value)}
+        value={value}
+        onChange={onChangeInput}
         className={styles.input}
         type='text'
         placeholder='Пошук піци...'
@@ -32,7 +48,7 @@ export const Search = () => {
         <path d='M 21 3 C 11.601563 3 4 10.601563 4 20 C 4 29.398438 11.601563 37 21 37 C 24.355469 37 27.460938 36.015625 30.09375 34.34375 L 42.375 46.625 L 46.625 42.375 L 34.5 30.28125 C 36.679688 27.421875 38 23.878906 38 20 C 38 10.601563 30.398438 3 21 3 Z M 21 7 C 28.199219 7 34 12.800781 34 20 C 34 27.199219 28.199219 33 21 33 C 13.800781 33 8 27.199219 8 20 C 8 12.800781 13.800781 7 21 7 Z' />
       </svg>
 
-      {searchValue && (
+      {value && (
         <svg
           onClick={onClickClear}
           className={styles.clear}
